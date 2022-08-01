d6f1e837ab745fdd5a06a34630e5f0e8f774ca83
Alhaytham Attia <aattia@ntgclarity.com>
2022-08-01 17:29:04 +0300

    login and registration activities

--- app/src/main/java/com/ntgclarity/authenticator/MainActivity.kt
+++ app/src/main/java/com/ntgclarity/authenticator/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.ntgclarity.authenticator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.ntgclarity.authenticator.words.WordsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        val btnRegistration = findViewById<Button>(R.id.btn_register)
+
+        btnRegistration.setOnClickListener {
+            startRegistration()
+        }
+
+        btnLogin.setOnClickListener {
+            startWords()
+        }
+    }
+
+    fun startRegistration() {
+        val intent = Intent(this, RegistrationActivity::class.java)
+
+        startActivity(intent)
+    }
+    fun startWords() {
+        val intent = Intent(this, WordsActivity::class.java)
+
+        startActivity(intent)
     }
 }
